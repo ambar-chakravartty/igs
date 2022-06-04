8d3d6247126742595e4c8a3cd6d5be6157b2fc63
sikrypt-team <sikrypt@gmail.com>
2022-06-04 11:44:28 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+
+app.set('view engine','ejs');
+app.set('views', './views');
+app.use(express.static('static'));
+
+app.get('/',(req,res)=>{
+	res.render('home')
+});
+
+app.get('/events',(req,res)=>{
+	res.render('events')
+});
+
+app.get('/register',(req,res)=>{
+	res.render('register')
+})
+
+
+app.listen(port,() =>{
+	console.log('listening on port : ',port);
+});
