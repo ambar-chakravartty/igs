b7b16ad96ef0e796164440682c5913a19ba8a662
sikrypt-team <sikrypt@gmail.com>
2022-06-16 21:48:45 +0530

    new site , basically

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 3000;
 app.use(express.static('public'));
 
 app.get('/',(req,res)=>{
-	res.sendFile(path.join(__dirname,'public/html/index.html'))
+	res.sendFile(path.join(__dirname,'public/html/index2.html'))
 });
 
 
